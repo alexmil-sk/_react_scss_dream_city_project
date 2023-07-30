9f4e6c3d6fff7cb75d4f5c6d3e29bb6b085ea8b0
alexmil-sk <alexmilweb@gmail.com>
2023-07-30 13:49:31 +0200

    Suspence Await for PostsPage is ready

--- src/pages/PostsPage/PostsPage.jsx
+++ src/pages/PostsPage/PostsPage.jsx
@@ -1,26 +1,21 @@
 import "./PostsPage.scss";
-import { Link, useSearchParams, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import { Link, useSearchParams, useLoaderData, Await, useAsyncValue } from "react-router-dom";
 import SearchComponent from "/src/components/Search/SearchComponent.jsx";
 
+//=========================================================================
 
-function PostsPage() {
+function MapPosts() {
 
-	const posts = useLoaderData();
+	const posts = useAsyncValue();
 	const [searchParams, setSearchParams] = useSearchParams();
 
 	const titleQuery = searchParams.get('title') || "";
-	const postIdQuery = searchParams.get('postId') || "";
 	const checkboxLatest = searchParams.has('latest') || "";
-	//url.ru/posts?post=abc&data=...&latest
 
 	const startsLatest = checkboxLatest ? 80 : 1;
 
-	function filterPosts(array) {
-		const newArray = array.filter(post => post.id == postIdQuery)
-		return mapPosts(newArray);
-	}
-
-	function mapPosts(array) {
+	function mapAllPosts(array) {
 		return (
 			array
 				.filter(post => post.title.includes(titleQuery?.toLowerCase()) && post.id >= startsLatest)
@@ -35,6 +30,53 @@ function PostsPage() {
 				))
 		)
 	}
+	return mapAllPosts(posts);
+}
+
+//==================================================================
+
+function FilterPosts() {
+
+	const posts = useAsyncValue();
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	const titleQuery = searchParams.get('title') || "";
+	const postIdQuery = searchParams.get('postId') || "";
+	const checkboxLatest = searchParams.has('latest') || "";
+
+	const startsLatest = checkboxLatest ? 80 : 1;
+
+	function filterAllPosts(array) {
+		const newArray = array.filter(post => post.id == postIdQuery)
+		return (
+			newArray
+				.filter(post => post.title.includes(titleQuery?.toLowerCase()) && post.id >= startsLatest)
+				.map(post => (
+					<Link to={`/posts/${post.id}`} key={post.id}>
+						<li
+							style={{ listStyle: 'square', lineHeight: 1.5, fontSize: '25px' }}
+						>
+							{post.userId}.{post.id} - {post.body}
+						</li>
+					</Link>
+				))
+		);
+	}
+	return filterAllPosts(posts)
+}
+
+//==================================================================
+
+
+function PostsPage() {
+
+	const { posts } = useLoaderData();
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	const titleQuery = searchParams.get('title') || "";
+	const postIdQuery = searchParams.get('postId') || "";
+	const checkboxLatest = searchParams.has('latest') || "";
+	//url.ru/posts?post=abc&data=...&latest
 
 	return (
 		<div className="posts">
@@ -59,11 +101,15 @@ function PostsPage() {
 					</button>
 				</Link>
 				<ul>
-					{
-						postIdQuery
-							? filterPosts(posts)
-							: mapPosts(posts)
-					}
+					<Suspense fallback={<h1 style={{ backgroundColor: 'yellow', margin: "20px 0" }}>Posts are loading...</h1>}>
+						<Await resolve={posts}	>
+							{
+								postIdQuery
+									? <FilterPosts />
+									: <MapPosts />
+							}
+						</Await>
+					</Suspense>
 				</ul>
 			</div>
 		</div>
@@ -71,10 +117,13 @@ function PostsPage() {
 	)
 }
 
-
-export const loaderPosts = async () => {
+async function getPosts() {
 	const res = await fetch('https://jsonplaceholder.typicode.com/posts');
 	return res.json();
+}
+
+export const loaderPosts = async () => {
+	return { posts: getPosts() };
 }
 
 export default PostsPage;
